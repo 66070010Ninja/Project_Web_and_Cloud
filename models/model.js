20a4153a84275285cf02c7f35a6f0db120ae3a4e
unknown <66070057@kmitl.ac.th>
2025-09-11 11:19:46 +0700

    update database

--- models/model.js
+++ models/model.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require('uuid');
 const Model = {
 
     getUser: (username, callback) => {
-        const sql = "SELECT * FROM users WHERE username=?";
+        const sql = "SELECT * FROM Account WHERE User_id=?";
         db.query(sql, [username], (err, results) => {
             if (err) return callback(err);
             callback(null, results[0]);
@@ -13,15 +13,15 @@ const Model = {
 
     postRegister: (username, password, callback) => {
         const id = uuidv4();
-        const sql = "INSERT INTO users (id, username, password) VALUES (?, ?, ?)";
-        db.query(sql, [id, username, password], (err, result) => {
+        const sql = "INSERT INTO Account (id, User_Name, Email, Password) VALUES (?, ?, ?, ?)";
+        db.query(sql, [id, username, Email, password], (err, result) => {
             if (err) return callback(err);
             callback(null, result);
         });
     },
 
     postLogin: (username, password, callback) => {
-        const sql = "SELECT * FROM users WHERE username=? AND password=?";
+        const sql = "SELECT * FROM Account WHERE User_Name=? AND Password=?";
         db.query(sql, [username, password], (err, results) => {
             if (err) return callback(err);
             callback(null, results[0]);
@@ -29,8 +29,8 @@ const Model = {
     },
     postUpload: (fileData, callback) => {
         const id = uuidv4();
-        const sql = "INSERT INTO files (id, filename, filepath) VALUES (?, ?, ?)";
-        db.query(sql, [id, fileData.filename, fileData.filepath], (err, result) => {
+        const sql = "INSERT INTO Game_Data (Game_id ,Game_Name, Description, Game_Image, File_Game, Status_Game, Log_Game) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        db.query(sql, [id, Game_Name, Description, Game_Image, File_Game, Status_Game, Log_Game], (err, result) => {
             if (err) return callback(err);
             callback(null, result);
         });
