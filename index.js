81112171808f55b02636f3daf0a2d7427178b3fd
66070010Ninja <66070010@kmitl.ac.th>
2025-09-12 16:13:08 +0700

    add .env

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 // index.js
 const express = require('express');
+require('dotenv').config();
+const mysql = require('mysql2');
 const app = express();
 const port = 3000;
+
+// MySQL database connection configuration
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
+// Connect to the database
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the MySQL database.');
+});
 
 // Basic route
 app.get('/', (req, res) => {
