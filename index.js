022035bd2252f45cd0102ccb035254274ab8eb6c
66070010Ninja <66070010@kmitl.ac.th>
2025-09-12 19:36:00 +0700

    add page

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
 // index.js
+const router = require('./router/page_router');
 const express = require('express');
-require('dotenv').config();
-const mysql = require('mysql2');
+
 const app = express();
+app.use(express.json());
+app.use('/api', router);
+
 const port = 3000;
-
-// MySQL database connection configuration
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
-
-// Connect to the database
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-    return;
-  }
-  console.log('Connected to the MySQL database.');
-});
-
-// Basic route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 // Start the server
 app.listen(port, () => {
