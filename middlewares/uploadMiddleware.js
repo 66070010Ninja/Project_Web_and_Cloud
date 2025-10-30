ea0eb907d06a0c4b0e976163040bfdbe138f44ae
66070010Ninja <66070010@kmitl.ac.th>
2025-10-30 20:27:56 +0700

    ,

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -47,7 +47,6 @@ const s3Storage = multerS3({
   s3: s3,
   bucket: process.env.AWS_S3_BUCKET_NAME,
   contentType: multerS3.AUTO_CONTENT_TYPE,
-  acl: "public-read",
   metadata: (req, file, cb) => {
     cb(null, { fieldName: file.fieldname });
   },
