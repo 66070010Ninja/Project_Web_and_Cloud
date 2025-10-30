8e1cd5e16447e9aa6494f50f3ceac4d77590a231
66070010Ninja <66070010@kmitl.ac.th>
2025-10-30 20:59:12 +0700

    .

--- controllers/gameControllers.js
+++ controllers/gameControllers.js
@@ -157,10 +157,10 @@ const gameController = {
                 updateData.File_Game = newGameFile.location;
             }
 
-            console.log("req.body['delete_images[]']:", req.body['delete_images[]']);
+            console.log("req.body['delete_images']:", req.body['delete_images']);
 
             // ✅ ลบรูปเก่า
-            let imagesToDelete = req.body["delete_images[]"] || [];
+            let imagesToDelete = req.body.delete_images || [];
             if (!Array.isArray(imagesToDelete)) imagesToDelete = [imagesToDelete];
 
             for (const imgId of imagesToDelete) {
